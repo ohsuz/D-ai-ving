399511da887945d75f65e70999d2db719e031d86
YUNJUNSEOK <roadvirushn@gmail.com>
2021-02-03 22:53:19 +0900

    taxifee update

--- Algorithms/합승 택시 요금/taxifee.py
+++ Algorithms/합승 택시 요금/taxifee.py
@@ -1,5 +1,17 @@
 def solution(n, s, a, b, fares):
-    answer = 0
+    answer = 1e10
+    graph = [[1e10 if i != j else 0 for i in range(n+1)] for j in range(n+1)]    
+    for fare in fares:
+        fr, to, fee = fare[0], fare[1], fare[2]
+        graph[fr][to] = fee
+        graph[to][fr] = fee
+    for mid in range(1, n+1):
+        for start in range(1, n+1):
+            for end in range(1, n+1):
+                if graph[start][end] > graph[start][mid] + graph[mid][end]:
+                    graph[start][end] = graph[start][mid] + graph[mid][end]   
+    for mid in range(1, n+1):
+        answer = min(answer, graph[s][mid]+graph[mid][a]+graph[mid][b])    
     return answer
 
 print(solution(6,4,6,2,[[4, 1, 10], [3, 5, 24], [5, 6, 2], [3, 1, 41], [5, 1, 24], [4, 6, 50], [2, 4, 66], [2, 3, 22], [1, 6, 25]]))
